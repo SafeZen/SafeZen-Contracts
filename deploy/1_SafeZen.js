26a5d7e83446e64f1d68a1913811554e21e645be
lewlian <seanlewtengsiong@gmail.com>
2022-05-20 12:56:50 +0800

    completed deployment files for mumbai

--- deploy/1_SafeZen.js
+++ deploy/1_SafeZen.js
@@ -5,26 +5,35 @@ require('dotenv').config();
 const gasLogger = new GasLogger();
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+	// Config
 	const { deploy } = deployments;
 	const { deployer } = await getNamedAccounts();
 	const chainId = await getChainId();
-	const HOST_ADDRESS = "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6";
-	const CFA = "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A";
-	const ACCEPTEDTOKEN = "0x15F0Ca26781C3852f8166eD2ebce5D18265cceb7";
-	// Config
-	console.log(`Deploying Mock Contracts... from ${deployer}`);
-	let govContract = await deploy('GovContract',{from: deployer});	
-	gasLogger.addDeployment(govContract);
-	let stakingContract = await deploy('StakingContract',{from: deployer});
-	gasLogger.addDeployment(stakingContract);
-	console.log(`Deploying SafeZen Contract... from ${deployer}`);
+	const HOST_ADDRESS = "0xEB796bdb90fFA0f28255275e16936D25d3418603";
+	const CFA = "0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873";
+	const ACCEPTEDTOKEN = "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"; //fDAIx
 
+	// Deployment
+	console.log(`Deploying Governance Contract... from ${deployer}`);
+	let govContract = await deploy('Governance',{from: deployer});	
+	gasLogger.addDeployment(govContract);
+
+	console.log(`Deploying Staking Contract... from ${deployer}`);
+	let stakingContract = await deploy('StakingContract',{
+		from: deployer ,
+		args:[
+			'SafeZenTokens', 'SZT'
+		]});
+	gasLogger.addDeployment(stakingContract);
+
+
+	console.log(`Deploying SafeZen Contract... from ${deployer}`);
 	let safezenContract = await deploy('SafeZen', {
 		from: deployer,
 		args: ['SafeZen', 'SZ', HOST_ADDRESS, CFA, ACCEPTEDTOKEN, stakingContract.address, govContract.address]
 	});
-
 	gasLogger.addDeployment(safezenContract);
+
 };
 
 module.exports.tags = ['SafeZen'];
